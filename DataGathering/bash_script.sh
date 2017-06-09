d151b7117451ab10bfc123db81673cc25be8bca7
erinversfeld <erinversfeld@gmail.com>
2017-06-09 13:42:35 +0200

    [bash script] need to run programs simultaneously

---
+++ DataGathering/bash_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# get participant number
+read number
+PARTICIPANT_NUMBER = $number
+
+# make directory
+mkdir PARTICIPANT_NUMBER
+cd PARTICIPANT_NUMBER
+
+# run programs
+C:\Users\Erin\Downloads\Myo\\ Data\\ Capture\\ Windows\Myo\\ Data\\ Capture.exe
+C:\Users\Erin\Downloads\android-studio-bundle-145.3537739-windows.exe
+C:\Users\Erin\Downloads\pycharm-professional-2016.3.2.exe
