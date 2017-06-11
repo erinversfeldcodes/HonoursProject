96e4aee4e1299e1a8d72dde89506892fa952c203
erinversfeld <erinversfeld@gmail.com>
2017-06-11 09:48:40 +0200

    [bash] loop until all gestures recorded

--- DataGathering/bash_script.sh
+++ DataGathering/bash_script.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+gesture_num = 0
+
 # run programs
+while [$gesture_num -lt 26]
+do
 .\\Myo\\MyoDataCapture & export MYO_PID=$!
 echo $MYO_PID
+echo "Myo data gatherer is running"
 read -r input
-if $input = "n"; then kill $MYO_PID fi
+if then $input = "n"; then kill $MYO_PID fi
+gesture_num = $[$gesture_num+1]
+done
