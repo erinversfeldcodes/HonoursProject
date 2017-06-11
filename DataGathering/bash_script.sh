13a0b92bb168530103ea96a0fdd771e0698a5d07
hannarst <rsthanna@gmail.com>
2017-06-11 10:41:26 +0200

    [bash] execute 26 times

--- DataGathering/bash_script.sh
+++ DataGathering/bash_script.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-gesture_num = 0
+gesture_num=0
 
 # run programs
-while [$gesture_num -lt 26]
+while [ $gesture_num -lt 26 ]
 do
-.\\Myo\\MyoDataCapture & export MYO_PID=$!
+python hello_world.py & export MYO_PID=$!
+#python ../../LEAP/Sample.py & export MYO_PID=$!
+#.\\Myo\\MyoDataCapture & export MYO_PID=$!
 echo $MYO_PID
 echo "Myo data gatherer is running"
 read -r input
-if then $input = "n"; then kill $MYO_PID fi
-gesture_num = $[$gesture_num+1]
+if [ "$input" = "n" ]
+then kill $MYO_PID
+fi
+gesture_num=$[$gesture_num+1]
 done
