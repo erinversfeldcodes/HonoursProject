86c9e5a63770775f740c7737eda10bdc82e53647
erinversfeld <erinversfeld@gmail.com>
2017-06-19 11:24:12 +0200

    [batch

--- DataGathering/batch_script.bat
+++ DataGathering/batch_script.bat
@@ -4,13 +4,25 @@ for /l %%x in (1, 1, 26) do (
     set /p choice1="Decide: ":
     if "%choice1%"=="m" (
       START py -2 ..\\..\\LEAP\\Sample.py meow%%x.txt
+      ECHO Leap data gatherer is running
+      START .\\Myo\\Myo\ Data\ Capture
       ECHO Myo data gatherer is running
+      START .\\Kinect\\KinectApplication\\KinectDataCapture
+      ECHO Kinect data gatherer is running
       set /p choice2="Enter to terminate... "
       taskkill /IM py.exe
+      taskkill /IM MyoDataCapture.exe
+      taskkill /IM KinectDataCapture.exe
     ) else (
       START py -2 ..\\..\\LEAP\\Sample.py meow%%x.txt
+      ECHO Leap data gatherer is running
+      START .\\Myo\\Myo\ Data\ Capture
       ECHO Myo data gatherer is running
+      START .\\Kinect\\KinectApplication\\KinectDataCapture
+      ECHO Kinect data gatherer is running
       timeout 3
       taskkill /IM py.exe
+      taskkill /IM MyoDataCapture.exe
+      taskkill /IM KinectDataCapture.exe
     )
 )
