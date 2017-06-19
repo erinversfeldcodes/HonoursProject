aa7bd77f9de364b220013824cdf875149ddd05b9
Anna <annasborysova@gmail.com>
2017-06-19 11:08:51 +0200

    [leap] add leap program

---
+++ DataGathering/Leap/config.py
@@ -0,0 +1,5 @@
+leap_lib = ".\\Leap_Motion_SDK_Windows_2.3.1\\LeapDeveloperKit_2.3.1+31549_win\\LeapSDK\\lib"
+more_leap = leap_lib + "\\x86"
+
+# leap_lib = "/mnt/c/Users/Anna/Downloads/Leap_Motion_SDK_Windows_2.3.1/LeapDeveloperKit_2.3.1+31549_win/LeapSDK/lib"
+# more_leap = leap_lib + "/x64"
