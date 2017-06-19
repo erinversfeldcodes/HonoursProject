dc2053d75fb04af14b23ad044b31726c55dcb0d0
erinversfeld <erinversfeld@gmail.com>
2017-06-19 12:22:40 +0200

    [batch] call correct programs from correct locations

--- DataGathering/batch_script.bat
+++ DataGathering/batch_script.bat
@@ -3,26 +3,26 @@ for /l %%x in (1, 1, 26) do (
     ECHO Enter to start recording for 3 seconds, enter 'm' for manual termination
     set /p choice1="Decide: ":
     if "%choice1%"=="m" (
-      START py -2 ..\\..\\LEAP\\Sample.py meow%%x.txt
+      START py -2 .\\Leap\\Sample.py meow%%x.txt
       ECHO Leap data gatherer is running
-      START .\\Myo\\Myo\ Data\ Capture
+      START .\\Myo\\MyoDataCapture
       ECHO Myo data gatherer is running
-      START .\\Kinect\\KinectApplication\\KinectDataCapture
+      START .\\Kinect\\KinectApplication\\kinectv2_viewer
       ECHO Kinect data gatherer is running
       set /p choice2="Enter to terminate... "
       taskkill /IM py.exe
       taskkill /IM MyoDataCapture.exe
-      taskkill /IM KinectDataCapture.exe
+      taskkill /IM kinectv2_viewer.exe
     ) else (
-      START py -2 ..\\..\\LEAP\\Sample.py meow%%x.txt
+      START py -2 .\\Leap\\Sample.py meow%%x.txt
       ECHO Leap data gatherer is running
-      START .\\Myo\\Myo\ Data\ Capture
+      START .\\Myo\\MyoDataCapture
       ECHO Myo data gatherer is running
-      START .\\Kinect\\KinectApplication\\KinectDataCapture
+      START .\\Kinect\\KinectApplication\\kinectv2_viewer
       ECHO Kinect data gatherer is running
       timeout 3
       taskkill /IM py.exe
       taskkill /IM MyoDataCapture.exe
-      taskkill /IM KinectDataCapture.exe
+      taskkill /IM kinectv2_viewer.exe
     )
 )
