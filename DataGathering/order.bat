77f4479c8aba94b90f7db15e345ead819bb636c1
erinversfeld <erinversfeld@gmail.com>
2017-07-07 05:00:10 +0200

    [batch] nah

--- DataGathering/order.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-for /l %%N in (1 1 50) do (
-    set "participant_file="
-    set /p participant_file="Participant (filename): "
-    python create_order.py %participant_file%
-)
-endlocal
-
