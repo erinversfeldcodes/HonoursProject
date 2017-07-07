e4a69afa683115b64159e148242678b5f3c59ab7
erinversfeld <erinversfeld@gmail.com>
2017-07-07 05:05:51 +0200

    [order] pre-generate the order for participants

--- DataGathering/create_order.py
+++ DataGathering/create_order.py
@@ -1,10 +1,11 @@
 import string, itertools, sys, random
 
-filename = sys.argv[1]
-order = list(itertools.repeat(list(string.ascii_lowercase), 10))
-flattened_order = [y for x in order for y in x]
-file = open(filename, 'w')
-random.shuffle(flattened_order)
-shuffled = ",".join(flattened_order)
-file.write(shuffled)
-sys.stdout.write(shuffled)
+for num in range(0, 50):
+	filename = str(num)
+	order = list(itertools.repeat(list(string.ascii_lowercase), 10))
+	flattened_order = [y for x in order for y in x]
+	file = open(filename, 'w')
+	random.shuffle(flattened_order)
+	shuffled = ",".join(flattened_order)
+	file.write(shuffled)
+	sys.stdout.write(shuffled)
