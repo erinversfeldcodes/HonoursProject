1c069fa4906327083d7ebfdf701389024bd1d896
erinversfeld <erinversfeld@gmail.com>
2017-07-16 14:29:20 +0200

    [batch] reconfigure for windows 8

--- DataGathering/batch_script.bat
+++ DataGathering/batch_script.bat
@@ -4,9 +4,8 @@ set "participant_file="
 set "list="
 setlocal EnableDelayedExpansion
 setlocal
-set /p participant_file="Participant (filename): "
-python create_order.py %participant_file%
-set /p list=<%participant_file%
+set /p participant_file="Participant number: "
+set /p list=<orders\%participant_file%
 echo %list%
 for %%x in (%list%) do (
     echo PERFORM GESTURE %%x
@@ -17,24 +16,24 @@ for %%x in (%list%) do (
       ECHO Myo data gatherer is running
       START .\\Kinect\\kinectv2_viewer %%x
       ECHO Kinect data gatherer is running
-      START python .\\Leap\\Sample.py Leap_%%x
+      START py -2 .\\Leap\\Sample.py Leap_%%x
       ECHO Leap data gatherer is running
       set /p choice2="Enter to terminate... "
       TASKKILL /IM MyoDataCapture.exe
       TASKKILL /IM kinectv2_viewer.exe
-      TASKKILL /IM python.exe
+      TASKKILL /IM py.exe
       TIMEOUT 1
     ) else (
       START .\\Myo\\MyoDataCapture
       ECHO Myo data gatherer is running
       START .\\Kinect\\kinectv2_viewer %%x
       ECHO Kinect data gatherer is running
-      START python .\\Leap\\Sample.py Leap_%%x
+      START py -2 .\\Leap\\Sample.py Leap_%%x
       ECHO Leap data gatherer is running
       TIMEOUT 3
       TASKKILL /IM MyoDataCapture.exe
       TASKKILL /IM kinectv2_viewer.exe
-      TASKKILL /IM python.exe
+      TASKKILL /IM py.exe
       TIMEOUT 1
     )
     endlocal
