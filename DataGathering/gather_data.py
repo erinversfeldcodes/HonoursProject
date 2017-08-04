576b8694281e7add2d36cd907dfc99992ab22a9c
erinversfeld <erinversfeld@gmail.com>
2017-08-04 22:14:59 +0200

    [data gathering, refactor, bug fix] pep 8 compliance and kill correct Kinect process

--- DataGathering/gather_data.py
+++ DataGathering/gather_data.py
@@ -2,23 +2,23 @@ import os
 import sys
 import time
 
-
 participant_number = sys.argv[1]
 round_number = sys.argv[2]
 
-with open('./orders/'+str(participant_number)+"-"+str(round_number)+".txt") as order_of_gestures:
-	gestures = list(order_of_gestures.read().replace(',', ''))
-	print gestures
-	for gesture in gestures:
-		os.system("START .\\Myo\\MyoDataCapture")
-		print "Myo data gatherer is running"
-		os.system("START .\\Kinect\\kinectv2_viewer(updatedv2) "+gesture)
-		print "Kinect data gatherer is running"
-		os.system("START .\\Leap\\Sample.py Leap_"+gesture)
-		print "Leap data gatherer is running"
-		time.sleep(3)
+with open('./orders/' + str(participant_number) + "-" + str(round_number) + ".txt") as order_of_gestures:
+    gestures = list(order_of_gestures.read().replace(',', ''))
+    print gestures
+    for gesture in gestures:
+        os.system("START .\\Myo\\MyoDataCapture")
+        print "Myo data gatherer is running"
+        os.system("START .\\Kinect\\kinectv2_viewer(updatedv2) " + gesture)
+        print "Kinect data gatherer is running"
+        os.system("START .\\Leap\\Sample.py Leap_" + gesture)
+        print "Leap data gatherer is running"
 
-		os.system("TASKKILL /IM MyoDataCapture.exe")
-		os.system("TASKKILL /IM kinectv2_viewer.exe")
-		os.system("TASKKILL /IM py.exe")
-		time.sleep(1)
+        time.sleep(3)
+
+        os.system("TASKKILL /IM MyoDataCapture.exe")
+        os.system("TASKKILL /IM kinectv2_viewer(updatedv2).exe ")
+        os.system("TASKKILL /IM Sample.exe")
+        time.sleep(1)
