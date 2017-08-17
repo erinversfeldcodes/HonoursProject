8fc0b88f8e5b3a142516e789f02e5271cc4fa9b4
erinversfeld <erinversfeld@gmail.com>
2017-08-17 08:45:16 +0200

    [spatial, ensemble] training classifiers on spatial data and workflow for ensemble method

--- Myo/__init__.py
+++ Myo/__init__.py
@@ -1,6 +1,6 @@
-from Myo.data_processing.pre_processing import *
 from Myo.data_processing.processing import *
 from Myo.data_processing.utils import *
+from Myo.ensemble_classifiers.voting import *
 import k_nearest_neighbour.k_nearest_neighbour as knn
 import artificial_neural_networks.artificial_neural_network as ann
 # import hidden_markov_models
@@ -10,7 +10,6 @@ import glob
 import os
 from sklearn.model_selection import train_test_split
 import time
-
 
 filename = "init_" + str(time.time())
 path_to_logs = os.path.abspath("log/")
@@ -37,66 +36,79 @@ def set_up():
     # the spatial data
     acc_regex = os.path.join(path_to_data_files, "*-accelerometer-*.csv")
     accelerometer_files = glob.glob(acc_regex)
-    acc_data = None
 
     gyro_regex = os.path.join(path_to_data_files, "*-gyro-*.csv")
     gyro_files = glob.glob(gyro_regex)
-    gyro_data = None
 
     orientation_regex = os.path.join(path_to_data_files, "*-orientation-*.csv")
     orientation_files = glob.glob(orientation_regex)
-    orientation_data = None
 
     orientation_euler_regex = os.path.join(path_to_data_files, "*-orientationEuler-*.csv")
     orientation_euler_files = glob.glob(orientation_euler_regex)
-    orientation_euler_data = None
+
+    best_spatial_classifier, spatial_params, spatial_accuracy = train_spatial_only(accelerometer_files, gyro_files,
+                                                                                   orientation_files,
+                                                                                   orientation_euler_files)
+
+    print("Best spatial classifier was: " + str(best_spatial_classifier) + ' with parameters ' + str(spatial_params) + ' achieving an accuracy of ' + str(spatial_accuracy))
 
     # the gestural data
-    logging.info(str(time.time()) + "beginning to process and train with emg data.")
     emg_regex = os.path.join(path_to_data_files, "*-emg-*.csv")
     emg_files = glob.glob(emg_regex)
-    train_gestural_only(emg_files)
+    best_gestural_classifier, gestural_params, gestural_accuracy = train_gestural_only(emg_files)
+    print("Best gestural classifier was: " + str(best_gestural_classifier) + ' with parameters ' + str(gestural_params) + ' achieving an accuracy of ' + str(spatial_accuracy))
 
-    return acc_data, gyro_data, orientation_data, orientation_euler_data
+    spatial_msg = 'Spatial classifier ' + str(best_spatial_classifier) + ' with params ' + str(
+        spatial_params) + 'performed best with accuracy score of ' + str(spatial_accuracy)
+    gestural_msg = 'Gestural classifier ' + str(best_gestural_classifier) + ' with params ' + str(
+        gestural_params) + 'performed best with accuracy score of ' + str(gestural_accuracy)
+    best_data_classifier_combo = spatial_msg if spatial_accuracy > gestural_accuracy else gestural_msg
+    print(best_data_classifier_combo)
+
+    ensemble = voting_ensemble_classifier(best_spatial_classifier, spatial_params, best_gestural_classifier,
+                                          gestural_params)
+    print("Ensemble accuracy: " + ensemble)
 
 
 def train_ann(x_train, x_test, y_train, y_test):
     max_accuracy = 0
-    # best_algorithm = None
+    best_algorithm = None
     best_combo = None
 
     # for i in range(10, 100):
     #     print(str(i) + " of 100.")
 
-    ann_accuracy, combo = ann.train(list(x_train), list(x_test), list(y_train), list(y_test))
+    ann_accuracy, algorithm, combo = ann.train(list(x_train), list(x_test), list(y_train), list(y_test))
 
     if ann_accuracy > max_accuracy:
         max_accuracy = ann_accuracy
-        # best_algorithm = algorithm
+        best_algorithm = algorithm
         best_combo = combo
 
     logging.info(str(time.time()) + ": ANN accuracy: " + str(max_accuracy) + ", with combination: " + str(best_combo))
-    return max_accuracy, best_combo
+    return max_accuracy, best_algorithm, best_combo
 
 
 def train_knn(x_train, x_test, y_train, y_test):
     max_accuracy = 0
+    best_algorithm = None
     best_combo = None
 
-    knn_accuracy, combo = knn.train(list(x_train), list(x_test), list(y_train), list(y_test))
+    knn_accuracy, algorithm, combo = knn.train(list(x_train), list(x_test), list(y_train), list(y_test))
 
     if knn_accuracy > max_accuracy:
         max_accuracy = knn_accuracy
+        best_algorithm = algorithm
         best_combo = combo
 
     logging.info(str(time.time()) + ": KNN accuracy: " + str(max_accuracy) + ", with combination: " + str(best_combo))
-    return max_accuracy, best_combo
+    return max_accuracy, best_algorithm, best_combo
 
 
 def train_gestural_only(emg_files):
-
     max_accuracy = 0
     best_algorithm = None
+    best_params = None
 
     emg_data = read_emg_data(emg_files, step=80)
     X = emg_data[0]
@@ -104,25 +116,59 @@ def train_gestural_only(emg_files):
 
     x_train, x_test, y_train, y_test = train_test_split(X, Y, random_state=0)
 
-    # ann_results = train_ann(x_train, x_test, y_train, y_test)
-    #
-    # if ann_results[0] > max_accuracy:
-    #     max_accuracy = ann_results[0]
-    #     best_algorithm = ("ANN", ann_results[1])
-
+    ann_results = train_ann(x_train, x_test, y_train, y_test)
     knn_results = train_knn(x_train, x_test, y_train, y_test)
+
+    if ann_results[0] > max_accuracy:
+        max_accuracy = ann_results[0]
+        best_algorithm = ann_results[1]
+        best_params = ann_results[2]
+
 
     if knn_results[0] > max_accuracy:
         max_accuracy = knn_results[0]
-        best_algorithm = ("KNN", knn_results[1])
+        best_algorithm = knn_results[1]
+        best_params = knn_results[2]
 
     # hmm_results = train_hmm(emg_files)
 
     logging.info(str(time.time()) + ": Best performance was: " + str(max_accuracy) + " from: " + str(best_algorithm))
 
+    return best_algorithm, best_params, max_accuracy
 
-def train_spatial_only(acc_data, gyro_data, orientation_data, orientation_euler_data):
-    pass
+
+def train_spatial_only(acc_files, gyro_files, orientation_files, orientation_euler_files):
+    max_accuracy = 0
+    best_algorithm = None
+    best_params = None
+
+    spatial_data = read_spatial_data(acc_files, gyro_files, orientation_files, orientation_euler_files, step=40)
+    X = spatial_data[0]
+    Y = spatial_data[1]
+
+    x_train, x_test, y_train, y_test = train_test_split(X, Y, random_state=0)
+
+    ann_results = train_ann(x_train, x_test, y_train, y_test)
+    knn_results = train_knn(x_train, x_test, y_train, y_test)
+
+    if ann_results[0] > max_accuracy:
+        max_accuracy = ann_results[0]
+        best_algorithm = ann_results[1]
+        best_params = ann_results[2]
+
+    if knn_results[0] > max_accuracy:
+        max_accuracy = knn_results[0]
+        best_algorithm = knn_results[1]
+        best_params = knn_results[2]
+
+    # hmm_results = train_hmm(emg_files)
+    #     max_accuracy = knn_results[0]
+    #     best_algorithm = knn_results[1]
+    #     best_params = knn[2]
+
+    logging.info(str(time.time()) + ": Best performance was: " + str(max_accuracy) + " from: " + str(best_algorithm))
+
+    return best_algorithm, best_params, max_accuracy
 
 
 if __name__ == "__main__":
