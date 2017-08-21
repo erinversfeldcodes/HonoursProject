884c1625caae901110060ac2111b5d89bb137600
erinversfeld <erinversfeld@gmail.com>
2017-08-21 11:13:32 +0200

    [ann, knn, refactor] return required values

--- Myo/artificial_neural_networks/artificial_neural_network.py
+++ Myo/artificial_neural_networks/artificial_neural_network.py
@@ -12,65 +12,60 @@ logging.info(str(time.time()) + " Loaded ann.py")
 
 
 def train(x_train, x_test, y_train, y_test):
+    """
+    Establish the best possible multilayer perceptron to model the given data and return the parameters used to create it as well as its accuracy
+    :param x_train: The data set with which to train the model
+    :param x_test: The data set with which to test the model
+    :param y_train: The target data set for training the model
+    :param y_test: The target data set with which to test the model
+    :return: The parameters used to crate the model and its accuracy
+    """
+    msg = str(time.time()) + ": Experimenting with ANNs"
+    print(msg)
+    logging.info(msg)
+
     activation_options = {'identity', 'logistic', 'tanh', 'relu'}
     solver_options = {'lbfgs', 'sgd', 'adam'}
     learning_rate_options = {'constant', 'invscaling', 'adaptive'}
     # TODO: Experiment with different alpha and hidden layer values
 
     max_accuracy = 0
-    best_algorithm = None
-    best_options = None
+    best_params = None
+    classifier = "MLP"
+    classifier_obj = None
 
     for activation in activation_options:
         for solver in solver_options:
             for learning_rate in learning_rate_options:
-                for hidden_layer_sizes in range(1, 20):
+                for hidden_layer_sizes in range(1, 2):  # 110):
 
+                    # create, train, score
                     multilayer_perceptron = neural_network.MLPClassifier(hidden_layer_sizes=(hidden_layer_sizes,),
                                                                          activation=activation, solver=solver,
                                                                          alpha=1e-5, learning_rate=learning_rate,
                                                                          early_stopping=True)
-                    try:
-                        multilayer_perceptron.fit(list(x_train), list(y_train))
+                    multilayer_perceptron.fit(list(x_train), list(y_train))
+                    mlp_accuracy = multilayer_perceptron.score(list(x_test), list(y_test))
 
-                        mlp_accuracy = multilayer_perceptron.score(list(x_test), list(y_test))
+                    # log results
+                    msg = str(time.time()) + ": Trained an ANN with options " + str(
+                        {"Hidden layer sizes: " + str(hidden_layer_sizes), "activation: " + str(activation),
+                         "solver: " + str(solver),
+                         " learning rate: " + str(learning_rate) + ", and it produced an accuracy score of " + str(
+                             mlp_accuracy) + '.'})
+                    logging.info(msg)
 
-                        # multilayer_regressor = neural_network.MLPRegressor(hidden_layer_sizes=(15,), activation=activation,
-                        #                                                    solver=solver, alpha=1e-5,
-                        #                                                    learning_rate=learning_rate, early_stopping=True)
-                        # best_algorithm,i multilayer_regressor.fit(list(x_train), list(y_train))
-                        #
-                        # mlpr_accuracy = multilayer_regressor.score(list(x), list(y))
-                        #
-                        #
-                        # if mlp_accuracy > mlpr_accuracy:
-                        #     accuracy = mlpr_accuracy
-                        #     algorithm = "MLP"
-                        #
-                        # else:
-                        #     accuracy = mlp_accuracy
-                        #     algorithm = "MLPR"
+                    # establish if there was an improvement, and document if necessary
+                    if mlp_accuracy > max_accuracy:
+                        max_accuracy = mlp_accuracy
+                        best_params = {"Layer sizes": hidden_layer_sizes, "Activation": activation,
+                                        "Solver": solver,
+                                        "Learning rate": learning_rate}
+                        classifier = "MLP"
+                        classifier_obj = multilayer_perceptron
 
-                        msg = str(time.time()) + ": Trained an ANN with options " + str(
-                            {"Hidden layer sizes: " + str(hidden_layer_sizes), "activation: " + str(activation),
-                             "solver: " + str(solver),
-                             " learning rate: " + str(learning_rate) + ", and it produced an accuracy score of " + str(
-                                 mlp_accuracy) + '.'})
-                        logging.info(msg)
-
-                        if mlp_accuracy > max_accuracy:
-                            max_accuracy = mlp_accuracy
-                            best_algorithm = "MLP"
-                            best_options = {"Layer sizes": hidden_layer_sizes, "Activation": activation,
-                                            "Solver": solver,
-                                            "Learning rate": learning_rate}
-
-                    except ValueError:
-                        print("X_train: " + str(list(x_train)))
-                        print("Y_train: " + str(list(y_train)))
-
-    msg = str(time.time()) + ': The best ANN was a ' + str(best_algorithm) + ' trained with the parameters' + str(
-        best_options) + ' which produced an accuracy score of: ' + str(max_accuracy)
+    # document which was best
+    msg = str(time.time()) + ': The best ANN was an MLP trained with the parameters' + str(best_params) + ' which produced an accuracy score of: ' + str(max_accuracy)
     print(msg)
     logging.info(msg)
-    return [max_accuracy, best_algorithm, best_options]
+    return {'Accuracy': max_accuracy, 'Classifier type': classifier, 'Params': best_params, 'Classifier obj': classifier_obj}
--- Myo/k_nearest_neighbour/k_nearest_neighbour.py
+++ Myo/k_nearest_neighbour/k_nearest_neighbour.py
@@ -12,44 +12,43 @@ logging.info("Loaded the Myo directory's __init__ script")
 
 
 def train(x_train, x_test, y_train, y_test):
-    # maybe use some smexy EC to optimise the number of neighbours?
+
+    msg = str(time.time()) + ": Experimenting with KNNs"
+    print(msg)
+    logging.info(msg)
 
     weights_options = {'uniform', 'distance'}
 
-    # p
-    # leaf size
-    # n_neighbours
-    # number_of_neighbours = 3
-
     max_accuracy = 0
-    best_combo = None
+    best_params = None
+    classifier = "KNN"
+    classifier_obj = None
 
+    # experiment with parameters
+    # TODO: establish how to vary the 'algorithm' parameter
     for weight in weights_options:
-        # # for algorithm in algorithm_options:
-        # #     for metric in metric_options:
-        #         if metric in {'seuclidean', 'mahalanobis'}:
-        #             metric_params = {'V': np.cov(X)}
-        #         else:
-        #             metric_params = None
-        for neighbour in range(3, 16):
-            for leaves in range(30, 50):
-                knn = KNeighborsClassifier(weights=weight, n_neighbors=neighbour, leaf_size=leaves)
+        for neighbour in range(2, 4):  # 10):
+            for leaves in range(3, 4):  # 50):
 
+                # create, train, fit
+                knn = KNeighborsClassifier(weights=weight, n_neighbors=neighbour, leaf_size=leaves)
                 knn.fit(list(x_train), list(y_train))
                 knn_accuracy = knn.score(list(x_test), list(y_test))
 
-                msg = str(time.time()) + ': Trained a KNN with the parameters' + str(("Weight: " + str(weight), "num neighbours: " + str(neighbour), "leaves: " + str(leaves)))
+                # document results
+                msg = str(time.time()) + ': Trained a KNN with the parameters' + str(("Weight: " + str(weight), "num neighbours: " + str(neighbour), "leaves: " + str(leaves) + " and it produced an accuracy score of " + str(knn_accuracy)))
                 logging.info(msg)
 
                 if knn_accuracy > max_accuracy:
                     max_accuracy = knn_accuracy
-                    algorithm = "KNN"
-                    best_combo = {"Weight": weight, "Num neighbours":  neighbour, "Leaves": leaves}
-                    msg = str(time.time()) + ': The best KNN thus far is one trained with the parameters' + str(best_combo) + ' which produced an accuracy score of: ' + str(max_accuracy)
+                    best_params = {"Weight": weight, "Num neighbours":  neighbour, "Leaves": leaves}
+                    classifier_obj = knn
+                    msg = str(time.time()) + ': The best KNN thus far is one trained with the parameters' + str(best_params) + ' which produced an accuracy score of: ' + str(max_accuracy)
                     logging.info(msg)
 
-    msg = str(time.time()) + ': The best KNN was one trained with the parameters' + str(best_combo) + ' which produced an accuracy score of: ' + str(max_accuracy)
+    msg = str(time.time()) + ': The best KNN was one trained with the parameters ' + str(best_params) + ' which produced an accuracy score of: ' + str(max_accuracy)
     print(msg)
     logging.info(msg)
 
-    return [max_accuracy, algorithm, best_combo]
+    return {'Accuracy': max_accuracy, 'Classifier type': classifier, 'Params': best_params, 'Classifier obj': classifier_obj}
+
